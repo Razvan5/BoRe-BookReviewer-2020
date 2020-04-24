73601d96455a62ed1101301a742bdb4aa40955a0
Adee <test@test>
2020-04-24 19:15:51 +0300

    Am reuat lucrul, am facut home, LogIn, LoggedIn, My Account, iar What's Hot e in lucru.

---
+++ js/BoRe_home_LoggedIn1.1.js
@@ -0,0 +1,7 @@
+var deschis=document.getElementById('deschis');
+deschis.style.transition="1s";
+    deschis.style.transform="rotateY(-145deg)";
+
+var card=document.getElementById('card');
+card.style.transition="1s";
+card.style.boxShadow="inset 20px 0 50px rgba(0,0,0,.5)";
